ea3330cebb2c0f8d9a6c64bcf9f5f64e2c065de6
Lucas Graças <lucasgracas@MacBook-Pro-de-Lucas.local>
2024-01-04 19:54:40 -0300

    projetos lukao

---
+++ projeto-injecao-de-dependencias/src/test/java/br/com/lukaoprogramador/projetoinjecaodedependencias/ProjetoInjecaoDeDependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lukaoprogramador.projetoinjecaodedependencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoInjecaoDeDependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
